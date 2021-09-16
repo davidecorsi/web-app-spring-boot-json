053be29eafff70668f598e61104913003630c493
Davide Corsi <davide.corsi@par-tec.it>
2021-09-16 10:48:39 +0200

    first commit

--- src/main/java/it/partec/webappspringbootjson/controller/StudentController.java
+++ src/main/java/it/partec/webappspringbootjson/controller/StudentController.java
@@ -5,15 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.partec.webappspringbootjson.model.Student;
+import it.partec.webappspringbootjson.dto.Student;
 import it.partec.webappspringbootjson.service.StudentService;
 
 @RestController
@@ -34,20 +32,6 @@ public class StudentController {
 		return new ResponseEntity<List<Student>>(studentList, HttpStatus.OK);
 	}
 	
-	@GetMapping("/{id}")
-	public ResponseEntity<Student> getStudent(@PathVariable("id") long id) {
-		Student student = null;
-		try {
-			student = studentService.getStudent(id);
-		} catch(Exception e) {
-			return new ResponseEntity<Student>(HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		if(student != null) {
-			return new ResponseEntity<Student>(student, HttpStatus.OK);
-		}
-		return new ResponseEntity<Student>(HttpStatus.NOT_FOUND);
-	}
-	
 	@PostMapping
 	public ResponseEntity<Object> addStudent(@RequestBody Student student) {
 		try {
@@ -56,9 +40,5 @@ public class StudentController {
 			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		return new ResponseEntity<>(HttpStatus.CREATED);
-	}
-	
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Object> deleteStudent(@PathVariable("id") long id) {
 	}
 }
--- src/main/java/it/partec/webappspringbootjson/service/StudentService.java
+++ src/main/java/it/partec/webappspringbootjson/service/StudentService.java
@@ -1,65 +1,12 @@
 package it.partec.webappspringbootjson.service;
 
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.IOException;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import it.partec.webappspringbootjson.dto.Student;
 
-import it.partec.webappspringbootjson.model.Student;
-
-@Service
-public class StudentService {
-
-	@Autowired
-	private ObjectMapper objectMapper;
-
-	public List<Student> getListStudent() throws Exception {
-		List<Student> studentList = null;
-		try(Reader file = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("liststudent.json"))) {
-			studentList = objectMapper.readValue(file, new TypeReference<List<Student>>(){});
-		} catch(Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-		return studentList;
-	}
-
-	public Student getStudent(long id) throws Exception {
-		List<Student> studentList = getListStudent();
-		Student student = null;
-		for(Student s: studentList) {
-			if(id == s.getId()) {
-				student = s;
-				break;
-			}
-		}
-		return student;
-	}
-
-	public void addStudent(Student student) throws Exception {
-		List<Student> studentList = getListStudent();
-		long id = 0;
-		for(Student s: studentList) {
-			if(s.getId() > id) {
-				id = s.getId();
-			}
-		}
-		student.setId(id + 1);
-		studentList.add(student);
-		try(Writer file = new PrintWriter(getClass().getClassLoader().getResource("liststudent.json").getFile())) {
-			file.write(objectMapper.writeValueAsString(studentList));
-		} catch(Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public void deleteStudent(long id) throws Exception{
-	}
+public interface StudentService {
+	
+	public List<Student> getListStudent() throws IOException;
+	public void addStudent(Student student) throws IOException;
 }
