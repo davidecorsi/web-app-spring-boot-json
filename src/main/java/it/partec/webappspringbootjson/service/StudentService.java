871ae62724b371d39ec86eda6c3db33d45e5c3b3
Davide Corsi <davide.corsi@par-tec.it>
2021-09-24 11:55:08 +0200

    profili

--- src/main/java/it/partec/webappspringbootjson/service/StudentService.java
+++ src/main/java/it/partec/webappspringbootjson/service/StudentService.java
@@ -7,8 +7,8 @@ import it.partec.webappspringbootjson.dto.Student;
 
 public interface StudentService {
 	
-	public List<Student> getListStudent() throws IOException;
-	public void addStudent(Student student) throws IOException;
-	public void deleteStudent(long id) throws IOException;
-	public Student getStudent(long id) throws IOException;
+	public List<Student> getListStudent() throws Exception;
+	public void addStudent(Student student) throws Exception;
+	public void deleteStudent(long id) throws Exception;
+	public Student getStudent(long id) throws Exception;
 }
